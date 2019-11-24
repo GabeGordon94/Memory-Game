af8ebcf4d13aee10b0be2d4526950ebbb4fcdcdf
Gabriel Gordon <gabegor94@gmail.com>
2019-11-24 21:23:08 +0200

    added features - new levels, counter and win page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-function newGame() {
-    board.empty();
-    openedCards = [];
-    score=0;
-    setUpEasyGame();
-}
 function shuffle(arr) {
     arr.sort(() => Math.random() - 0.5);
     return arr;
@@ -23,59 +17,137 @@ function flip(e) {
         score++;
         console.log('same');
         console.log(score);
-        if(score==6){
-            alert('you won');
+        if (score == maxScore) {
+            winScreen();
         }
         //clear array
-        openedCards=[];
+        openedCards = [];
     } else {
         console.log('wrong');
-        $('.card').css('pointer-events','none');
-        setTimeout(function(e){
+        guesses++;
+        $('#counter').text(guesses);
+        $('.card').css('pointer-events', 'none');
+        setTimeout(function (e) {
             console.log('timeout');
             $(ele).addClass('backgroundCard');
             $(openedCards[0]).addClass('backgroundCard');
-            $('.card').css('pointer-events','');
-            openedCards=[]
-        },1000)
+            $('.card').css('pointer-events', '');
+            openedCards = []
+        }, 1000)
 
     }
 
 }
 
 function setUpEasyGame() {
+    maxScore=6;
+    guesses=0;
+    $('#counter').text(null);
+    board.empty();
+    openedCards = [];
+    score = 0;
+    $('#popup').hide();
     let easyImages = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg'];
     var cards = easyImages;
     cards = cards.concat(easyImages);
     cards = shuffle(cards);
     for (var i = 0; i < cards.length; i++) {
         let card = document.createElement('div');
-        card.className = 'card backgroundCard';
+        card.className = 'card cardEasy backgroundCard';
         //card.style.backgroundImage=`url('img/${backgroundImg}')`;
         card.addEventListener('click', flip);
         card.style.backgroundImage = `url('img/${cards[i]}')`;
         board.append(card);
     }
+    board.show();
 }
 function setUpMediumGame() {
-    let mediumImages = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg'];
-    var cards = easyImages;
-    cards = cards.concat(easyImages);
+    maxScore=9;
+    guesses=0;
+    $('#counter').text(null);
+    board.empty();
+    openedCards = [];
+    score = 0;
+    $('#popup').hide();
+
+    let mediumImages = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg', 'seven.jpg', 'eight.jpg', 'nine.jpg'];
+    var cards = mediumImages;
+    cards = cards.concat(mediumImages);
     cards = shuffle(cards);
     for (var i = 0; i < cards.length; i++) {
         let card = document.createElement('div');
-        card.className = 'card backgroundCard';
+        card.className = 'card cardMedium backgroundCard';
         //card.style.backgroundImage=`url('img/${backgroundImg}')`;
         card.addEventListener('click', flip);
         card.style.backgroundImage = `url('img/${cards[i]}')`;
         board.append(card);
     }
+    board.show();
 }
 
+
+function setUpHardGame() {
+    maxScore=12;
+    guesses=0;
+    $('#counter').text(null);
+    board.empty();
+    openedCards = [];
+    score = 0;
+    $('#popup').hide();
+
+    let hardImages = ['one.jpg', 'two.jpg', 'three.jpg', 'four.jpg', 'five.jpg', 'six.jpg', 'seven.jpg',
+     'eight.jpg', 'nine.jpg','ten.jpg','eleven.jpg','twelve.jpg'];
+    var cards = hardImages;
+    cards = cards.concat(hardImages);
+    cards = shuffle(cards);
+    for (var i = 0; i < cards.length; i++) {
+        let card = document.createElement('div');
+        card.className = 'card cardHard backgroundCard';
+        //card.style.backgroundImage=`url('img/${backgroundImg}')`;
+        card.addEventListener('click', flip);
+        card.style.backgroundImage = `url('img/${cards[i]}')`;
+        board.append(card);
+    }
+    board.show();
+}
+
+
+function showOptions() {
+    board.hide()
+    $('#win').hide();
+    $('#counter').text(null);
+    $('#popup').show();
+    
+}
+
+function popUpNewGame() {
+    $('#win').hide();
+    let $popUp = $('#popup');
+    $popUp.append($('<p>Welcome to Nature!</p>'));
+    $popUp.append($('<button class="btnOption" id="easy" onclick="setUpEasyGame()">Easy</button>'));
+    $popUp.append($('<button class="btnOption" id="medium" onclick="setUpMediumGame()">Medium</button>'));
+    $popUp.append($('<button class="btnOption" id="hard" onclick="setUpHardGame()">Hard</button>'));
+}
+
+function winScreen(){
+    board.hide();
+    let $winScr=$('#win');
+    $winScr.empty();
+    $winScr.append($('<h1>You won!</h1>'));
+    $winScr.append($(`<h3>It took you ${guesses} guesses!</h3>`));
+    $winScr.append($('<button onclick="showOptions()">Play again</button>'));
+
+    $winScr.show();
+
+}
+
+
 let openedCards = [];
-let score=0;
+let score = 0;
+let guesses=0;
+let maxScore;
 const backgroundImg = 'background.jpg';
 let board = $('#playField');
-
-$('#newGameBtn').on('click', newGame);
+popUpNewGame();
+$('#newGameBtn').on('click', showOptions);
 
